23382ca47dd62c99d97253b5930e5fd1c483a8d4
Muhammad Rehan Khan <101788037+RehanKhan14@users.noreply.github.com>
2023-10-25 08:37:15 +0500

    fixed source target issues

--- project.py
+++ project.py
@@ -1,6 +1,29 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+# Create a directed graph
+G = nx.DiGraph()
+
+# Add nodes with positions
+G.add_node('A', pos=(0, 0))
+G.add_node('B', pos=(2, 2))
+G.add_node('C', pos=(2, -2))
+G.add_node('D', pos=(4, 0))
+
+# Add edges with weights
+G.add_edge('A', 'B', weight=4, capacity=10)
+G.add_edge('A', 'C', weight=2, capacity=5)
+G.add_edge('B', 'C', weight=5, capacity=15)
+G.add_edge('B', 'D', weight=10, capacity=10)
+G.add_edge('C', 'D', weight=3, capacity=10)
+
+# Set node positions
+pos = nx.get_node_attributes(G, 'pos')
+
+# Source and target nodes
+source_node = 'A'
+target_node = 'D'
+
 # Function to find the shortest path using Dijkstra's algorithm
 def shortest_path(graph, source, target):
     shortest_path = nx.shortest_path(graph, source=source, target=target, weight='weight')
@@ -9,25 +32,8 @@ def shortest_path(graph, source, target):
 
 # Function to find the maximum flow using Ford-Fulkerson algorithm
 def maximum_flow(graph, source, target):
-    flow_value, flow_dict = nx.maximum_flow(graph, source=source, sink=target)
+    flow_value, flow_dict = nx.maximum_flow(graph, source, target)
     return flow_value, flow_dict
-
-# Create a directed graph
-G = nx.DiGraph()
-
-# Add edges with weights and coordinates
-G.add_edge('A', 'B', weight=4, capacity=10, pos={'A': (0, 0), 'B': (2, 2)})
-G.add_edge('A', 'C', weight=2, capacity=5, pos={'A': (0, 0), 'C': (2, -2)})
-G.add_edge('B', 'C', weight=5, capacity=15, pos={'B': (2, 2), 'C': (2, -2)})
-G.add_edge('B', 'D', weight=10, capacity=10, pos={'B': (2, 2), 'D': (4, 0)})
-G.add_edge('C', 'D', weight=3, capacity=10, pos={'C': (2, -2), 'D': (4, 0)})
-
-# Set node positions
-pos = nx.get_edge_attributes(G, 'pos')
-
-# Source and target nodes
-source_node = 'A'
-target_node = 'D'
 
 # Find the shortest path
 shortest_path, shortest_distance = shortest_path(G, source_node, target_node)
